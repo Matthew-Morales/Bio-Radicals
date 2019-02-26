d30c5d5af37c732b9ca28e1560ca047e69d2c670
amocz <iamchristopherzhao@gmail.com>
2019-02-25 23:17:32 -0800

    added #mos dat'g left formula, changed date format, formated cells

--- reports_generator.py
+++ reports_generator.py
@@ -1,6 +1,7 @@
 import xlsxwriter
 from pathlib import Path    # pathlib uses the correct format of path for the local OS
 import os
+from datetime import datetime
 
 def reserve_report(customer_id, conn):
     filename = 'customer' + str(customer_id) + '.xlsx'
@@ -14,6 +15,19 @@ def reserve_report(customer_id, conn):
 
     workbook = xlsxwriter.Workbook(file_path)   # create excel sheet
     worksheet = workbook.add_worksheet()
+
+    # set column width
+    worksheet.set_column(1, 1, 40)
+    worksheet.set_column(2, 7, 10)
+    worksheet.set_column(9, 9, 40)
+    worksheet.set_row(5, 45)
+
+    # set format
+    cell_format = workbook.add_format()
+    cell_format.set_align('center')
+    cell_format.set_align('vcenter')
+    cell_format.set_text_wrap()
+
     sheet_row = 0
     sheet_col = 0
     worksheet.write(sheet_row, sheet_col, "Subject:")  # row 1
@@ -26,23 +40,24 @@ def reserve_report(customer_id, conn):
     worksheet.write(sheet_row, sheet_col, "")   # row 3
     sheet_row += 1
 
+    date_format = workbook.add_format({'num_format': 'mm/dd/yy'})
     worksheet.write(sheet_row, sheet_col, "Date:")  # row 4
-    worksheet.write(sheet_row, sheet_col + 1, "Date")
+    worksheet.write(sheet_row, sheet_col + 1, "=TODAY()", date_format)
     sheet_row += 1
 
     worksheet.write(sheet_row, sheet_col, "")  # row 5
     sheet_row += 1
 
-    worksheet.write(sheet_row, sheet_col, "ITEM #")  # row 6
-    worksheet.write(sheet_row, sheet_col + 1, "DESCRIPTION")
-    worksheet.write(sheet_row, sheet_col + 2, "QTY/BOX")
-    worksheet.write(sheet_row, sheet_col + 3, "LOT #")
-    worksheet.write(sheet_row, sheet_col + 4, "EXP. DATE")
-    worksheet.write(sheet_row, sheet_col + 5, "Product Allocation")
-    worksheet.write(sheet_row, sheet_col + 6, "Shipped Quantity (Indy)")
-    worksheet.write(sheet_row, sheet_col + 7, "Remaining Allocation")
-    worksheet.write(sheet_row, sheet_col + 8, "#mos dat'g left")
-    worksheet.write(sheet_row, sheet_col + 9, "Comments")
+    worksheet.write(sheet_row, sheet_col, "ITEM #", cell_format)  # row 6
+    worksheet.write(sheet_row, sheet_col + 1, "DESCRIPTION", cell_format)
+    worksheet.write(sheet_row, sheet_col + 2, "QTY/BOX", cell_format)
+    worksheet.write(sheet_row, sheet_col + 3, "LOT #", cell_format)
+    worksheet.write(sheet_row, sheet_col + 4, "EXP. DATE", cell_format)
+    worksheet.write(sheet_row, sheet_col + 5, "Product Allocation", cell_format)
+    worksheet.write(sheet_row, sheet_col + 6, "Shipped Quantity (Indy)", cell_format)
+    worksheet.write(sheet_row, sheet_col + 7, "Remaining Allocation", cell_format)
+    worksheet.write(sheet_row, sheet_col + 8, "#mos dat'g left", cell_format)
+    worksheet.write(sheet_row, sheet_col + 9, "Comments", cell_format)
     sheet_row += 1
 
     worksheet.write(sheet_row, sheet_col, "")  # row 7
@@ -50,8 +65,7 @@ def reserve_report(customer_id, conn):
 
     cur.execute(
         """
-        select p.id, p.name, 'qty/box', l.id, l.expiration_date, r.booked_qty, r.shipped_qty, r.remaining_qty,
-        printf("%.1f",(julianday(l.expiration_date) - julianday(date('now')))/30.42)
+        select p.id, p.name, 'qty/box', l.id, l.expiration_date, r.booked_qty, r.shipped_qty, r.remaining_qty
         from product p 
         left join lot l on l.product_id = p.id
         left join reservation r on r.batch_id = l.id
@@ -73,16 +87,16 @@ def reserve_report(customer_id, conn):
     rows = cur.fetchall()   # return list of items from the query
     for row in rows:
         print(row)
-
+        date_time = datetime.strptime(str(row[4]).split(' ')[0], '%Y-%m-%d')    #set date format
         worksheet.write(sheet_row, sheet_col, str(row[0]))
         worksheet.write(sheet_row, sheet_col + 1, str(row[1]))
         worksheet.write(sheet_row, sheet_col + 2, str(row[2]))
         worksheet.write(sheet_row, sheet_col + 3, str(row[3]))
-        worksheet.write(sheet_row, sheet_col + 4, str(row[4]).split(' ')[0])
+        worksheet.write(sheet_row, sheet_col + 4, date_time, date_format)
         worksheet.write(sheet_row, sheet_col + 5, str(row[5]))
         worksheet.write(sheet_row, sheet_col + 6, str(row[6]))
         worksheet.write(sheet_row, sheet_col + 7, str(row[7]))
-        worksheet.write(sheet_row, sheet_col + 8, str(row[8]))
+        worksheet.write(sheet_row, sheet_col + 8, '=ROUND(((-TODAY()) + $E%d)/30.42,1)' % (sheet_row+1))    # formula to calculate mos dat'g left
         worksheet.write(sheet_row, sheet_col + 9, "")
         sheet_row += 1
 
